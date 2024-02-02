9fa6da385711442f29611efb0524f5e748fab312
gianlucanied <gianluca.nieddu96@gmail.com>
2024-02-02 16:02:17 +0100

    Add: bonus 1

--- index.php
+++ index.php
@@ -4,22 +4,32 @@ class Movie {
     public $titolo;
     public $regista;
     public $durata;
+    public $genere;
 
-    public function __construct($titolo, $regista, $durata) {
+    public function __construct($titolo, $regista, $durata, $genere) {
 
         $this -> titolo = $titolo;
         $this -> regista = $regista;
         $this -> durata = $durata;
+        $this -> genere = $genere;
     }
 
     public function getRegista() {
 
         return $this -> regista;
     }
+
+    public function getGenere() {
+
+        return $this -> genere;
+    }
 }
 
-$movie1 = new Movie("La fabbrica di cioccolato", "Tim Burton", "1h 55m");
-$movie2 = new Movie("Avatar", "James Cameron", "2h 41m");
+$genereMovie1 = ["Famiglia", "Avventura"];
+$genereMovie2 = ["Fantascienza"];
+
+$movie1 = new Movie("La fabbrica di cioccolato", "Tim Burton", "1h 55m", $genereMovie1);
+$movie2 = new Movie("Avatar", "James Cameron", "2h 41m", $genereMovie2);
 
 
 var_dump($movie1 -> titolo);
@@ -27,6 +37,8 @@ echo "<br>";
 var_dump($movie1 -> regista);
 echo "<br>";
 var_dump($movie1 -> durata);
+echo "<br>";
+var_dump($movie1 -> getGenere());
 
 echo "<br><br>";
 
@@ -35,3 +47,5 @@ echo "<br>";
 var_dump($movie2 -> regista);
 echo "<br>";
 var_dump($movie2 -> durata);
+echo "<br>";
+var_dump($movie2 -> getGenere());
